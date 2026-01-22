8675a266bcaa6611010c51093ce36c41083781b1
Omolaiye Samuel Oluwatobi <somolaiye53@gmail.com>
2026-01-22 11:35:02 +0100

    Fix build + update site title

--- app/components/FileUpload.tsx
+++ app/components/FileUpload.tsx
@@ -34,12 +34,11 @@ export default function FileUpload({
                 resourceType: resourceType,
                 folder: 'resources',
                 tags: ['public', 'samchurch'],
-                useAssetFolderAsPublicIdPrefix: false,
                 useFilename: true,
                 uniqueFilename: false,
                 clientAllowedFormats: resourceType === 'image' ? ['png', 'jpeg', 'jpg', 'webp', 'pdf'] : undefined,
                 maxFileSize: resourceType === 'image' ? 20000000 : 100000000, // 20MB for image/pdf, 100MB others
-            }}
+            } as any}
             onSuccess={handleUpload}
         >
             {({ open }) => {
